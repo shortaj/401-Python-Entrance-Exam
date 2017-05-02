b6af71056c717a5cc70b753ffa4f06d27b94bd71
Alex <ajshort2010@hotmail.com>
2017-05-02 03:25:03 -0700

    well I have it working localhost now. Shouldn't be much longer till deployed to Heroku...

--- index.js
+++ index.js
@@ -3,5 +3,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/Public'));
+app.get(express.static('/Resources/Images/1493167447_facebook_square.png'))
+
+
 const server = app.listen(3000, function() {
-  console.log('listening on port 3000');
+  console.log('listening on port 3000')
+});
