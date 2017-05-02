4f9baa8b94b7a24a96901d9ba898a22b41ed0cc5
Alex <ajshort2010@hotmail.com>
2017-05-02 03:41:04 -0700

    fiddling with server files.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const express = require('express');
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/Public'));
 app.get(express.static('/Resources/Images/1493167447_facebook_square.png'))
 
 
-const server = app.listen(3000, function() {
-  console.log('listening on port 3000')
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
